992cf75bbc8f1b859e2a0a452214a40e09fb9589
Aswathi1302 <aswathisomaraj1302@gmail.com>
2022-11-06 13:41:46 +0530

    inserted

--- src/bloodbank.java
+++ src/bloodbank.java
@@ -1,7 +1,19 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.util.Scanner;
 
 public class bloodbank {
     public static void main(String[] args) {
+        String donername,place,bloodgroup;
+        int bid,phone,age;
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con= DriverManager.getConnection("jdbc:mysql://localhost:3306/bloodbank1","root","");
+        }
+        catch (Exception e){
+            System.out.println((e));
+        }
         int choice;
         while (true)
         {
@@ -19,7 +31,36 @@ public class bloodbank {
             switch (choice)
             {
                 case 1:
-                    System.out.println("Add Details");
+                    System.out.println("Enter the blood id");
+                    bid=sc.nextInt();
+                    System.out.println("Enter the Doner name");
+                    donername=sc.next();
+                    System.out.println("Enter the place");
+                    place=sc.next();
+                    System.out.println("Enter the blood group");
+                    bloodgroup=sc.next();
+                    System.out.println("Enter the age");
+                    age=sc.nextInt();
+                    System.out.println("Enter the phone");
+                    phone=sc.nextInt();
+                    try{
+                        Class.forName("com.mysql.jdbc.Driver");
+                        Connection con= DriverManager.getConnection("jdbc:mysql://localhost:3306/bloodbank1","root","");
+                        String sql="INSERT INTO `bloodbank`(`bid`, `donername`, `place`, `bloodgroup`, `age`, `phone`) VALUES (?,?,?,?,?,?)";
+                        PreparedStatement stmt=con.prepareStatement((sql));
+                        stmt.setInt(1,bid);
+                        stmt.setString(2,donername);
+                        stmt.setString(3,place);
+                        stmt.setString(4,bloodgroup);
+                        stmt.setInt(5,age);
+                        stmt.setInt(6,phone);
+                        stmt.executeUpdate();
+                        System.out.println("value inserted successfully.........!");
+                    }
+                    catch (Exception e){
+                        System.out.println((e));
+                    }
+
                     break;
                 case  2:
                     System.out.println("view all details of blood table");
